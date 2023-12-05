4fc50993fb6f99d733d4fb2e3f384155b763df26
shivam2403 <136799994+shivam2403@users.noreply.github.com>
2023-12-06 04:26:43 +0530

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,19 @@
 const router=require('express').Router();
 const Category=require('../models/Category');
+const cors = require('cors');
+
+// Apply CORS middleware for the entire router
+router.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);
 
 router.post('/',async(req,res)=>{
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+    
     const newCat=new Category(req.body);
     try {
         const savedCat=await newCat.save();
@@ -12,6 +24,8 @@ router.post('/',async(req,res)=>{
 })
 
 router.get('/',async(req,res)=>{
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
     try {
         const cats=await Category.find();
         return res.status(200).json(cats);
